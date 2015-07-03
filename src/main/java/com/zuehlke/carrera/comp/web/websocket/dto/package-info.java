212f33a456fcdd73533d62b4ca3911fe8c15bae7
Wolfgang Giersche <wgiersche@gmail.com>
2015-07-03 11:15:49 +0200

    some clean-up and suppressed security for RESTful calls between server instances (needs be looked at!)

--- src/main/java/com/zuehlke/carrera/comp/web/websocket/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Access Objects used by WebSocket services.
- */
-package com.zuehlke.carrera.comp.web.websocket.dto;
--- src/main/java/com/zuehlke/carrera/comp/web/websocket/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * WebSocket services, using Spring Websocket.
- */
-package com.zuehlke.carrera.comp.web.websocket;
