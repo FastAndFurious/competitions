65a2bede6ae9692ffccd5c3fb9d9757d53995fb1
Wolfgang Giersche <wgiersche@gmail.com>
2015-11-29 18:10:32 +0100

    Score Board style

--- src/main/webapp/scripts/app/scoreboard/scoreBoard.js
+++ src/main/webapp/scripts/app/scoreboard/scoreBoard.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('scoreBoardApp', ['LocalStorageModule', 'tmh.dynamicLocale',
-    'ngResource', 'ui.router', 'ngCookies', 'pascalprecht.translate', 'ngCacheBuster', 'infinite-scroll'])
+        'ngResource', 'ui.router', 'ngCookies', 'pascalprecht.translate', 'ngCacheBuster', 'infinite-scroll'])
 
-    .run(function ( $state) {
+    .run(function ($state) {
         $state.go('scoreBoard')
     })
 
@@ -25,13 +25,13 @@ angular.module('scoreBoardApp', ['LocalStorageModule', 'tmh.dynamicLocale',
                     controller: 'ScoreBoardController'
                 }
             },
-            onEnter: function(Status, Round) {
+            onEnter: function (Status, Round) {
                 Status.connect();
                 Round.connect();
                 Status.subscribe();
                 Round.subscribe();
             },
-            onExit: function(Status, Round) {
+            onExit: function (Status, Round) {
                 Status.unsubscribe();
                 Round.unsubscribe();
                 Status.disconnect();
@@ -43,35 +43,91 @@ angular.module('scoreBoardApp', ['LocalStorageModule', 'tmh.dynamicLocale',
 
     })
 
-    .controller('ScoreBoardController', function ( $scope, Status, $interval) {
+    .controller('ScoreBoardController', function ($scope, Status, $interval) {
 
         $scope.currentRoundElapsed = 0;
         $scope.currentRoundStart = Date.now();
         $scope.currentRunElapsed = 0;
         $scope.currentRunStart = Date.now();
 
-        $scope.display = "MAIN";
+        $scope.display = "ONGOING";
 
         $scope.title = "Competition not yet started";
 
+        $scope.status = {
+            name: "HSR Challenge 2015",
+            currentSession: "Competition No 1",
+            currentBoard: [
+                {
+                    position: 1,
+                    team: "kobayashi",
+                    duration: 5960
+                },
+                {
+                    position: 2,
+                    team: "skyfall",
+                    duration: 6239
+                },
+                {
+                    position: 3,
+                    team: "amidala",
+                    duration: 7023
+                },
+                {
+                    position: 4,
+                    team: "schumi",
+                    duration: 7100
+                },
+                {
+                    position: 5,
+                    team: "geronimo",
+                    duration: 7301
+                }
+            ],
+            recentRunInfo: {
+                team: "kobayashi",
+                bestOfThisTeam: {
+                    position: 2,
+                    duration: 5678
+                },
+                currentRunResults: [
+                    {
+                        duration: 5678
+                    },
+                    {
+                        duration: 6100
+                    },
+                    {
+                        duration: 6200
+                    },
+                    {
+                        duration: 7101
+                    },
+                    {
+                        duration: 7234
+                    }
+                ]
+            }
+        };
+
         var stopRun;
 
-        Status.receive().then(null, null, function(status) {
+        Status.receive().then(null, null, function (status) {
             $scope.status = status;
 
-            if ( status.recentRunInfo == null ) {
-                if ( $scope.display !== "MAIN" ) {
+            if (status.recentRunInfo == null) {
+                if ($scope.display !== "MAIN") {
                     $scope.display = "MAIN";
-                    if ( angular.isDefined(stopRun)) {
+                    if (angular.isDefined(stopRun)) {
                         $interval.cancel(stopRun);
                         stopRun = undefined;
                     }
                 }
             } else {
                 $scope.currentRoundStart = Date.now();
-                if ( $scope.display !== "ONGOING" ) {
+                if ($scope.display !== "ONGOING") {
                     $scope.currentRunStart = Date.now();
-                    stopRun = $interval(function(){
+                    stopRun = $interval(function () {
                         $scope.currentRoundElapsed = ( Date.now() - $scope.currentRoundStart ) / 1000.0;
                         $scope.currentRunElapsed = ( Date.now() - $scope.currentRunStart ) / 1000.0;
                     }, 10);
